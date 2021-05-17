aa7ed455e90b1de8a267987a0dacf3a2f1df3327
sbeysbey <skoru1972@gmail.com>
2021-05-17 11:31:55 +0200

    index

--- 32_JS_Java_Sscript--10/script.js
+++ 32_JS_Java_Sscript--10/script.js
@@ -1,4 +1,4 @@
-var istiklalmarsi= "<h3 style='text-align:center'>İSTİKLAL MARŞI </h3> <br>Korkma, sönmez bu şafaklarda yüzen al sancak.<br/> Sönmeden yurdumun üstünde tüten en son ocak.<br/> O benim milletimin yıldızıdır parlayacak!<br/> O benimdir, o benim milletimindir ancak!<br/><br/> Çatma, kurban olayım, çehreni ey nazlı hilal!<br/> Kahraman ırkıma bir gül..ne bu şiddet, bu celâl?<br/> Sana olmaz dökülen kanlarımız sonra helal.<br/>Hakkıdır, Hakk'a tapan milletimin istiklal.</br></br><div style='text-align:center'>  MEHMET AKİF ERSOY</div>"
+var istiklalmarsi= "<h3 style='text-align:left'>İSTİKLAL MARŞI </h3> <br>Korkma, sönmez bu şafaklarda yüzen al sancak.<br/> Sönmeden yurdumun üstünde tüten en son ocak.<br/> O benim milletimin yıldızıdır parlayacak!<br/> O benimdir, o benim milletimindir ancak!<br/><br/> Çatma, kurban olayım, çehreni ey nazlı hilal!<br/> Kahraman ırkıma bir gül..ne bu şiddet, bu celâl?<br/> Sana olmaz dökülen kanlarımız sonra helal.<br/>Hakkıdır, Hakk'a tapan milletimin istiklal.</br></br><div style='text-align:center'>  MEHMET AKİF ERSOY</div>"
 
 function goster(){
   document.getElementById("iccerceve").innerHTML=istiklalmarsi;
