b4892fa4b454bbda44370edaa551856b3e356a9b
sbsysbey <skoru1972@gmail.com>
2021-10-24 00:00:10 +0200

    ipad

---
+++ İpad/script.js
@@ -0,0 +1,46 @@
+var ikon1=document.querySelector(".ikon1");
+var ikon2=document.querySelector(".ikon2");
+var ikon3=document.querySelector(".ikon3");
+var ikon4=document.querySelector(".ikon4");
+var ikon5=document.querySelector(".ikon5");
+var ikon6=document.querySelector(".ikon6");
+var ikon7=document.querySelector(".ikon7");
+var ikon8=document.querySelector(".ikon8");
+var ikon9=document.querySelector(".ikon9");
+var ikon10=document.querySelector(".ikon10");
+var ikon11=document.querySelector(".ikon11");
+var ikon12=document.querySelector(".ikon12");
+var ikon13=document.querySelector(".ikon13");
+var ikon14=document.querySelector(".ikon14");
+
+
+ikon1.onclick=function(){
+    window.open("https://apple.com");
+}
+ikon2.onclick=function(){
+    window.open("https://www.youtube.com/watch?v=NRSkIULLy7M");
+}
+ikon3.onclick=function(){
+    window.open("https://www.google.com.tr/maps/");
+}
+ikon4.onclick=function(){
+    window.open("https://web.whatsapp.com/");
+}
+ikon5.onclick=function(){
+    window.open("https://mail.google.com/mail/");
+}
+ikon6.onclick=function(){
+    window.open("C:/Users/MRN/IdeaProjects/HTML_CSS/İpad/depo/");
+}
+ikon7.onclick=function(){
+    window.open("C:/Users/MRN/IdeaProjects/HTML_CSS/İpad/depo/");
+}
+ikon8.onclick=function(){
+    window.open("C:/Users/MRN/IdeaProjects/HTML_CSS/İpad/depo/");
+}
+ikon9.onclick=function(){
+    window.open("C:/Users/MRN/IdeaProjects/HTML_CSS/İpad/depo/");
+}
+ikon10.onclick=function(){
+    window.open("C:/Users/MRN/IdeaProjects/HTML_CSS/İpad/depo/");
+}
