f913f6246b3174452b398a5b276fb8c98b3ace5d
sbsysbey <skoru1972@gmail.com>
2021-10-24 15:00:26 +0200

    ipad

--- İpad/script.js
+++ İpad/script.js
@@ -12,35 +12,85 @@ var ikon11=document.querySelector(".ikon11");
 var ikon12=document.querySelector(".ikon12");
 var ikon13=document.querySelector(".ikon13");
 var ikon14=document.querySelector(".ikon14");
+var home=document.querySelector(".homebutton");
 
+home.onclick=function(){
+    document.querySelector(".ekranfr").style.zIndex="1";
+    document.querySelector.src="";
+    document.querySelector(".ekran").style.zIndex="4";
+    document.querySelector(".ipadresim").style.zIndex="2";
+    
+}
 
 ikon1.onclick=function(){
-    window.open("https://apple.com");
+
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="C:/Users/MRN/Videos/Man.mp4";
 }
 ikon2.onclick=function(){
-    window.open("https://www.youtube.com/watch?v=NRSkIULLy7M");
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="../20_CSS_satranc/satranc.html";
 }
+
 ikon3.onclick=function(){
-    window.open("https://www.google.com.tr/maps/");
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="../22_JS_SifrerGizleme/index.html";
 }
 ikon4.onclick=function(){
-    window.open("https://web.whatsapp.com/");
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="../33_JS_kutuOlusturma/index.html";
 }
 ikon5.onclick=function(){
-    window.open("https://mail.google.com/mail/");
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="C:/Users/MRN/Videos/Man.mp4";
 }
 ikon6.onclick=function(){
-    window.open("C:/Users/MRN/IdeaProjects/HTML_CSS/İpad/depo/");
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="https://sqlzoo.net/wiki/SELECT_Quiz";
 }
 ikon7.onclick=function(){
-    window.open("C:/Users/MRN/IdeaProjects/HTML_CSS/İpad/depo/");
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="https://www.ogretmenevde.com.tr/etkinlik/category/";
 }
 ikon8.onclick=function(){
-    window.open("C:/Users/MRN/IdeaProjects/HTML_CSS/İpad/depo/");
+    var depo=document.querySelector(".ekranfr").style.zIndex="3";
+    var ekran = document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="depo";
 }
 ikon9.onclick=function(){
-    window.open("C:/Users/MRN/IdeaProjects/HTML_CSS/İpad/depo/");
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="C:/Users/MRN/Videos/Man.mp4";
 }
 ikon10.onclick=function(){
-    window.open("C:/Users/MRN/IdeaProjects/HTML_CSS/İpad/depo/");
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="C:/Users/MRN/Videos/Man.mp4";
+}
+ikon11.onclick=function(){
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="C:/Users/MRN/Videos/Man.mp4";
+}
+ikon12.onclick=function(){
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="C:/Users/MRN/Videos/Man.mp4";
+}
+ikon13.onclick=function(){
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="C:/Users/MRN/Videos/Man.mp4";
+}
+ikon14.onclick=function(){
+    document.querySelector(".ekranfr").style.zIndex="3";
+    document.querySelector(".ekran").style.zIndex="1";
+    document.querySelector(".ekranfr").src="C:/Users/MRN/Videos/Man.mp4";
 }
